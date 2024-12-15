793fb81197ced17c3ad367989d28e01b037a3ff8
杨梦雨 <2168526598@qq.com>
2024-12-15 19:36:10 +0800

    init

---
+++ traffic_client.sh
@@ -0,0 +1,1 @@
+python -m traffic_generation.run_clients --dst_ip 10.0.5.2 --src_ports 11000,11040 --dst_ports 11000,11040 --ipd 1 --duration 100 --log_dir log_traffic/
---
+++ traffic_server.sh
@@ -0,0 +1,1 @@
+python -m traffic_generation.run_servers --ports 11000,11040 --log_dir log_traffic
